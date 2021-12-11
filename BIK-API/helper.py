e35d414269db255da1d1619e65b6d2d431a44878
adam <korba.adam@gmail.com>
2021-12-11 15:04:40 +0100

    Add API communication

---
+++ BIK-API/helper.py
@@ -0,0 +1,19 @@
+import requests
+
+root = "https://gateway.oapi.bik.pl/"
+
+def requestBIK(street, buildingNumber, postalCode, url):
+    url = root + url
+    head = {"Content-Type": "application/json","Cache-Control": "no-cache", "BIK-OAPI-Key": "876dc7308eda42958239852af8b1f2db"}
+    body = {
+        "size": "500",
+        "address": {
+            "code": f"{postalCode}",
+            "city": "ŁÓDŹ",
+            "street": f"{street}",
+            "buildingNumber": f"{buildingNumber}"
+        },
+        "demographicData": "M16_POPT"
+    }
+    r = requests.post(url, headers=head, json=body, cert=("cert.pem", "server.key"), verify=False)
+    return r
