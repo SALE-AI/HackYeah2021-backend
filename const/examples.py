8108bdbdfad67980814d8cb80df0ec1309c7386d
adam <korba.adam@gmail.com>
2021-12-11 17:09:09 +0100

    Add example data

---
+++ const/examples.py
@@ -0,0 +1,102 @@
+def getUczelnie():
+    return """{
+    "inputDataAddress": {
+        "code": "92221",
+        "city": "Łódź",
+        "street": "NOWOGRODZKA",
+        "building_number": "17"
+    },
+    "size": "500",
+    "nearestPOI": {
+        "D_EDUKACJA_WYZSZE_SZKOLY_PUBLICZNE": 810
+    }
+}"""
+
+def getSzkolyPodstawowe():
+    return """{
+    "inputDataAddress": {
+        "code": "92221",
+        "city": "Łódź",
+        "street": "NOWOGRODZKA",
+        "building_number": "17"
+    },
+    "size": "500",
+    "nearestPOI": {
+        "D_EDUKACJA_SZKOLY_PODSTAWOWE": 700
+    }
+}"""
+
+def getPrzedszkola():
+    return """{
+    "inputDataAddress": {
+        "code": "92221",
+        "city": "Łódź",
+        "street": "NOWOGRODZKA",
+        "building_number": "17"
+    },
+    "size": "500",
+    "nearestPOI": {
+        "D_EDUKACJA_PRZEDSZKOLA_I_PUNKTY_PRZEDSZKOLNE": 480
+    }
+}"""
+
+def getKawiarnie():
+    return """{
+    "inputDataAddress": {
+        "code": "92221",
+        "city": "Łódź",
+        "street": "NOWOGRODZKA",
+        "building_number": "17"
+    },
+    "size": "500",
+    "nearestPOI": {
+        "D_HORECA_KAWIARNIA": 1160
+    }
+}"""
+
+def getRestauracje():
+    return """{
+    "inputDataAddress": {
+        "code": "92221",
+        "city": "Łódź",
+        "street": "NOWOGRODZKA",
+        "building_number": "17"
+    },
+    "size": "100",
+    "nearestPOI": {
+        "D_HORECA_RESTAURACJA": 890
+    }
+}"""
+
+def getPoczta():
+    return """{
+    "inputDataAddress": {
+        "code": "92221",
+        "city": "Łódź",
+        "street": "NOWOGRODZKA",
+        "building_number": "17"
+    },
+    "size": "100",
+    "nearestPOI": {
+        "D_POCZTA": 1720
+    }
+}"""
+
+def getPaczkomat():
+    return """{
+    "inputDataAddress": {
+        "code": "92221",
+        "city": "Łódź",
+        "street": "NOWOGRODZKA",
+        "building_number": "17"
+    },
+    "size": "100",
+    "nearestPOI": {
+        "D_PRZESYLKI_PACZKOMAT": 430
+    }
+}
+    """
+
+
+def getPrzystanki():
+    return """"""
