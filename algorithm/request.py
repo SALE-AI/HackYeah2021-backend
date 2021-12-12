5a57738fab48536cf5b0948598ee201c076795fd
adam <korba.adam@gmail.com>
2021-12-12 02:21:17 +0100

    fix

--- algorithm/request.py
+++ algorithm/request.py
@@ -1,9 +1,17 @@
 import requests
+import os
+from os.path import join, dirname
+from dotenv import load_dotenv
 
 root = "https://gateway.oapi.bik.pl/"
 
 def requestBIK(body, url):
     url = root + url
-    head = {"Content-Type": "application/json","Cache-Control": "no-cache", "BIK-OAPI-Key": "240f1bd2fb374ed2aa28b20315528bcc"}
+    
+    dotenv_path = join(dirname(__file__), '.env')
+    load_dotenv(dotenv_path)
+    key = os.environ.get('BIK_OAPI_KEY')
+
+    head = {"Content-Type": "application/json","Cache-Control": "no-cache", "BIK-OAPI-Key": key}
     r = requests.post(url, headers=head, json=body, cert=("cert.pem", "server.key"), verify=False)
     return r.json()
